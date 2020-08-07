2dd9831d6f1b32c1bfc1c9d371669c9b34984918
ImShEthan <48135622+ImShEthan@users.noreply.github.com>
2020-08-07 20:52:57 +0200

    Add files via upload

---
+++ radio.js
@@ -0,0 +1,26 @@
+const { Client } = require('discord.js');
+const client = new Client();
+var request = require('request');
+
+
+const broadcast = client.voice.createBroadcast();
+
+
+let servid = "ID" // Server ID
+let chanid = "ID" // Channel ID
+let flux = "http://direct.mouv.fr/live/mouv-midfi.mp3" //flux link
+let token = "TOKEN" // Your token
+
+
+
+client.on('ready', () => {
+	console.log("Bot ready")
+	client.user.setActivity("Dev by Ethan")
+	client.set
+	client.guilds.cache.get(servid).channels.cache.get(chanid).join()
+	.then(connection => {
+		broadcast.play(flux);
+		connection.play(broadcast, { volume: 0.1 });
+	})
+});
+client.login(token);
